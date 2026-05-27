501c2dd543649929330c18a76cdc913c0d396bea
Taay Sosa <mateoghost3@gmail.com>
2026-05-27 10:06:35 +0200

    Inital summative commit with Section B, Question 1 completed. Console Application that recieves input then displays the programs structure

---
+++ ProgramStructureDemo/ProgramStructureDemo/ProgramStructureDemo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class ProgarmStructureDemo
+{
+    static void Main(string[] args)
+    {
+        // Programs welcome message
+        Console.WriteLine("Welcome to the Structure of a C# Program Demo!");
+
+        // Prompt user to enter a students name
+        Console.Write("Enter students name: ");
+
+        // Variable to store student name
+        string username = Console.ReadLine();
+
+        // Prints the username
+        Console.WriteLine($"Hello {username}!");
+
+        // Prints the programs structure
+        Console.WriteLine("\n1. using System : imports functionality");
+        Console.WriteLine("2. namespace : organizes code");
+        Console.WriteLine("3. class ProgarmStructureDemo  : container for code");
+        Console.WriteLine("4. Main()  : entry point for code");
+        Console.WriteLine("5. Comments  : explain logic and documentation");
+    }
+}
